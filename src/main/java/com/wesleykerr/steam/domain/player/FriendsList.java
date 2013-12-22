3315eb607dc06ccad79daaef6470b28c3b88d13d
Wesley Kerr <wesley.n.kerr@gmail.com>
2013-12-22 09:42:58 -0800

    Updated existing with Gson and added FriendsList
    Created builder objects and made immutable.
    Added annotations to keep variable names consistent with Java standards

---
+++ src/main/java/com/wesleykerr/steam/domain/player/FriendsList.java
@@ -0,0 +1,133 @@
+package com.wesleykerr.steam.domain.player;
+
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.gson.annotations.SerializedName;
+import com.wesleykerr.steam.domain.player.Player.Builder;
+import com.wesleykerr.utils.GsonUtils;
+
+public class FriendsList {
+
+    @SerializedName("_id")
+    private String id;
+    @SerializedName("_rev")
+    private String rev;
+
+    private List<Relationship> friendsList;
+    private long updateDateTime;
+    
+    public FriendsList() { 
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @return the rev
+     */
+    public String getRev() {
+        return rev;
+    }
+
+    /**
+     * @return the friendsList
+     */
+    public List<Relationship> getFriendsList() {
+        return friendsList;
+    }
+
+    /**
+     * @return the updateDateTime
+     */
+    public long getUpdateDateTime() {
+        return updateDateTime;
+    }
+
+    public static class Relationship {
+
+        private String steamid;
+        private String relationship;
+        
+        @SerializedName("friend_since") 
+        private Long friendSince;
+        
+        public Relationship() { 
+            
+        }
+
+        /**
+         * @return the steamid
+         */
+        public String getSteamid() {
+            return steamid;
+        }
+
+        /**
+         * @return the relationship
+         */
+        public String getRelationship() {
+            return relationship;
+        }
+
+        /**
+         * @return the friendSince
+         */
+        public Long getFriendSince() {
+            return friendSince;
+        }
+        
+        @Override
+        public String toString() { 
+            return GsonUtils.getDefaultGson().toJson(this);
+        }
+        
+    }
+    
+    public static class Builder { 
+        private FriendsList friendsList;
+        
+        private Builder() { 
+            friendsList = new FriendsList();
+        }
+        
+        public Builder withId(String id) {
+            friendsList.id = id;
+            return this;
+        }
+        
+        public Builder withRev(String rev) { 
+            friendsList.rev = rev;
+            return this;
+        }
+        
+        public Builder withFriends(List<Relationship> friends) { 
+            friendsList.friendsList = Lists.newArrayList(friends);
+            return this;
+        }
+        
+        public Builder withUpdateDateTime(long updateDateTime) { 
+            friendsList.updateDateTime = updateDateTime;
+            return this;
+        }
+
+        public FriendsList build() { 
+            Preconditions.checkNotNull(friendsList);
+            
+            FriendsList tmp = friendsList;
+            friendsList = null;
+            return tmp;
+        }
+        
+        public static Builder create() { 
+            return new Builder();
+        }
+    }
+
+}
--- src/main/java/com/wesleykerr/steam/domain/player/Player.java
+++ src/main/java/com/wesleykerr/steam/domain/player/Player.java
@@ -1,16 +1,24 @@
 package com.wesleykerr.steam.domain.player;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.gson.annotations.SerializedName;
 
 
 public class Player {
 	
-	private String _id;
-	private String _rev;
+    @SerializedName("_id")
+	private String id;
+
+    @SerializedName("_rev")
+	private String rev;
 	
 	private List<GameStats> games;
+
 	private long updateDateTime;
+	private long friendsMillis;
 	
 	private boolean visible;
 	
@@ -21,26 +29,8 @@ public class Player {
 	/**
 	 * @return the _id
 	 */
-	public String get_id() {
-		return _id;
-	}
-
-	/**
-	 * @param _id the _id to set
-	 */
-	public void set_id(String _id) {
-		this._id = _id;
-	}
-
-
-	/**
-	 * Add a game for this player.
-	 * @param stats
-	 */
-	public void addGame(GameStats stats) { 
-		if (games == null) 
-			games = new ArrayList<GameStats>();
-		games.add(stats);
+	public String getId() {
+		return id;
 	}
 
 	/**
@@ -51,24 +41,10 @@ public class Player {
 	}
 
 	/**
-	 * @param games the games to set
-	 */
-	public void setGames(List<GameStats> games) {
-		this.games = games;
-	}
-
-	/**
 	 * @return the updateDateTime
 	 */
 	public long getUpdateDateTime() {
 		return updateDateTime;
-	}
-
-	/**
-	 * @param updateDateTime the updateDateTime to set
-	 */
-	public void setUpdateDateTime(long updateDateTime) {
-		this.updateDateTime = updateDateTime;
 	}
 
 	/**
@@ -79,24 +55,71 @@ public class Player {
 	}
 
 	/**
-	 * @param visible the visible to set
-	 */
-	public void setVisible(boolean visible) {
-		this.visible = visible;
-	}
-
-	/**
 	 * @return the _rev
 	 */
-	public String get_rev() {
-		return _rev;
+	public String getRev() {
+		return rev;
 	}
 
-	/**
-	 * @param _rev the _rev to set
-	 */
-	public void set_rev(String _rev) {
-		this._rev = _rev;
+
+	public static class Builder {
+	    
+	    private Player player;
+	    
+	    private Builder() { 
+	        player = new Player();
+	    }
+	    
+	    public Builder withPlayer(Player p) {
+	        player.id = p.id;
+	        player.rev = p.rev;
+	        player.games = Lists.newArrayList(p.games);
+	        player.updateDateTime = p.updateDateTime;
+	        player.friendsMillis = p.friendsMillis;
+	        player.visible = p.visible;
+	        return this;
+	    }
+	    
+	    public Builder withId(String id) {
+	        player.id = id;
+	        return this;
+	    }
+	    
+	    public Builder withRev(String rev) { 
+	        player.rev = rev;
+	        return this;
+	    }
+	    
+	    public Builder withGames(List<GameStats> games) { 
+	        player.games = Lists.newArrayList(games);
+	        return this;
+	    }
+	    
+	    public Builder isVisible(boolean visible) {
+	        player.visible = visible;
+	        return this;
+	    }
+	    
+	    public Builder withUpdateDateTime(long updateDateTime) { 
+	        player.updateDateTime = updateDateTime;
+	        return this;
+	    }
+	    
+	    public Builder withFriendsMillis(long friendsMillis) { 
+	        player.friendsMillis = friendsMillis;
+	        return this;
+	    }
+	    
+	    public Player build() { 
+	        Preconditions.checkNotNull(player);
+	        
+	        Player tmp = player;
+	        player = null;
+	        return tmp;
+	    }
+	    
+	    public static Builder create() { 
+	        return new Builder();
+	    }
 	}
-	
 }
