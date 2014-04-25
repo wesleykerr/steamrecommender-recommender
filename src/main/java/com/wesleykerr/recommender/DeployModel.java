bbfb90ba724112ebff0cf86535e90eafebb13133
Wesley Kerr <wesley.n.kerr@gmail.com>
2014-04-25 06:59:35 -0700

    reverted to the right version of DeployModel

--- src/main/java/com/wesleykerr/recommender/DeployModel.java
+++ src/main/java/com/wesleykerr/recommender/DeployModel.java
@@ -4,6 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.wesleykerr.steam.persistence.MySQL;
@@ -86,11 +95,63 @@ public class DeployModel {
     public List<Long> getColumns() { 
         return columns;
     }
+
+    public static Options getOptions() { 
+    	Options options = new Options();
+    	
+    	@SuppressWarnings("static-access")
+		Option input = OptionBuilder
+    			.withLongOpt("input")
+    			.withArgName("input")
+    			.hasArg()
+    			.isRequired()
+    			.create("i");
+    	options.addOption(input);
+
+    	@SuppressWarnings("static-access")
+		Option output = OptionBuilder
+    			.withLongOpt("modelid")
+    			.withArgName("modelId")
+    			.hasArg()
+    			.isRequired()
+    			.create("m");
+    	options.addOption(output);
+    	
+    	@SuppressWarnings("static-access")
+    	Option help = OptionBuilder
+    			.withLongOpt("help")
+    			.withArgName("help")
+    			.create("h");
+    	options.addOption(help);
+
+    	return options;
+    }
+    
+    public static void printHelp(Options options) { 
+    	HelpFormatter formatter = new HelpFormatter();
+    	formatter.printHelp( "DeployModel", options );
+    }
+    
     
     public static void main(String[] args) throws Exception { 
-        // TODO read these in as a parameters
+    	Options options = getOptions();
+    	CommandLineParser parser = new BasicParser();
+    	
         int modelId = 1;  
         String input = "/data/steam/model.csv";
+    	try {
+    		CommandLine line = parser.parse(options, args);
+    		if (line.hasOption("h")) {
+    			printHelp(options);
+    			System.exit(1);
+    		}
+    		
+        	modelId = Integer.parseInt(line.getOptionValue("m"));
+        	input = line.getOptionValue("i");
+    	} catch (ParseException exp) { 
+    		printHelp(options);
+    		System.exit(1);
+    	}
         
         DeployModel deployModel = new DeployModel(modelId);
         deployModel.loadMatrix(input);
